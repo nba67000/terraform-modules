47c2d08701f3f1119f7bf1787c3e485eb98162d9
Nicolas Barbier <nba67000@gmail.com>
2022-12-10 14:07:06 +0100

    create security groups

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -31,3 +31,9 @@ module "nat_gateway" {
   private_data_subnet_az2_id  = module.vpc.private_data_subnet_az2_id
 }
 
+# create security groups
+module "security_groups" {
+  source                      = "../modules/security_groups"
+  vpc_id                      = module.vpc.vpc_id
+}
+
