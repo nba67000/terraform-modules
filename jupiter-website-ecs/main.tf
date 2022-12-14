a5017c72193641b677d4b73467ee10f913309770
Nicolas Barbier <nba67000@gmail.com>
2022-12-14 08:38:48 +0100

    create acm

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -43,3 +43,10 @@ module "ecs_task_execution_role" {
   project_name  = module.vpc.project_name
 }
 
+# create ssl certificate from ACM
+module "acm" {
+  source            = "../modules/acm"
+  domain_name       = var.domain_name
+  alternative_name  = var.alternative_name
+}
+
--- modules/ecs_task_execution_role/outputs.tf
+++ modules/ecs_task_execution_role/outputs.tf
@@ -1,3 +1,3 @@
 output "ecs_tasks_execution_role_arn" {
- value = aws_iam_role_policy_attachment.ecs_tasks_execution_role.arn
+ value = aws_iam_role.ecs_tasks_execution_role.arn
 }
